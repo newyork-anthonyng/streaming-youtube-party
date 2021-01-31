e8b07e1c3932c850af4170676159e869a8c33b15
Anthony Ng <newyork.anthonyng@gmail.com>
2021-01-31 12:42:33 -0500

    add socket

--- public/app.js
+++ public/app.js
@@ -1,1 +1,23 @@
 console.log('app.js loaded!');
+
+const socket = io();
+
+const $input = document.querySelector('.js-input');
+const $submit = document.querySelector('.js-button');
+const $msgContainer = document.querySelector('.js-msg');
+
+$submit.addEventListener('click', function() {
+  const text = $input.value;
+
+  socket.emit('TEST_CHANNEL', {
+    id: Date.now(),
+    text: text
+  });
+});
+
+socket.on('TEST_CHANNEL_2', (data) => {
+  const $newMsg = document.createElement('div');
+  $newMsg.innerHTML = JSON.stringify(data, null, 2);
+
+  $msgContainer.appendChild($newMsg);
+});
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('TEST_CHANNEL', (data) => {
+
+    io.emit('TEST_CHANNEL_2', data);
+    console.log('***********');
+    console.log(data);
+    console.log('***********');
+  });
+});
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
@@ -9,6 +28,6 @@ app.get('/health', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
