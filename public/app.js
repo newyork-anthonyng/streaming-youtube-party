928b34b8c538f26781785265498c619cd5cec00e
Anthony Ng <newyork.anthonyng@gmail.com>
2021-02-06 10:32:33 -0500

    add scrubber

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+/*
+ * 1. when the Youtube player is playing, update the scrubber
+ *      player.getCurrentTime() // duration in seconds
+ *      player.getDuration() // duration in seconds
+ * 2. when user updates the scrubber, we should update the youtube player
+ *    player.seekTo
+*/
 function loadYoutubeApi() {
   const tag = document.createElement('script');
 
@@ -49,6 +56,8 @@ const socket = io();
 const $input = document.querySelector('.js-input');
 const $submit = document.querySelector('.js-button');
 const $play = document.querySelector('.js-play-button');
+const $scrubber = document.querySelector('.js-scrubber');
+const $scrubberInfo = document.querySelector('.js-scrubber-time');
 
 $submit.addEventListener('click', function() {
   const url = $input.value;
@@ -80,16 +89,47 @@ $play.addEventListener('click', function() {
   }
 });
 
+let scrubberTimer;
+function formatTime(duration)
+{
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
 socket.on('VIDEO:PLAY', (data) => {
   player.playVideo();
   isPlaying = true;
   $play.innerText = 'Pause';
+
+  scrubberTimer = setInterval(() => {
+    const currentTime = player.getCurrentTime();
+    const totalDuration = player.getDuration();
+    $scrubberInfo.innerText = `${formatTime(currentTime)} / ${formatTime(totalDuration)}`;
+
+    const percentage =  currentTime/totalDuration ;
+    $scrubber.value =  percentage * 100;
+  }, 1000);
 });
 
 socket.on('VIDEO:PAUSE', (data) => {
   player.pauseVideo();
   isPlaying = false;
   $play.innerText = 'Play';
+
+  clearInterval(scrubberTimer);
 });
 
 socket.on('VIDEO:INIT', (data) => {
