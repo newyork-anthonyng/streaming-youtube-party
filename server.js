2b68ace54ad446e08ce7ab7933217c42f29c79d9
Anthony Ng <newyork.anthonyng@gmail.com>
2021-02-07 10:48:24 -0500

    prepare for heroku deployment

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/health', (req, res) => {
   res.json({ ok: true });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
